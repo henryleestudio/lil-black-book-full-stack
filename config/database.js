87a1bb4fd9c83b3f397ac1ab605ba88490bffea8
Henry Lee - Dev <henrylee.contact@gmail.com>
2022-06-14 10:32:17 -0400

    Adding file for new repo

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://blk:blk123@lil-black-book.1hbm7.mongodb.net/blkBook?retryWrites=true&w=majority', 
+    'dbName': 'blkBook'
+};
